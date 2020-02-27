2f4117085b3d9f27e1f68786d5b0f78888698631
Valentin Markin <jobedano@gmail.com>
2020-02-27 17:23:51 +0400

    custom render format added

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,10 +2,12 @@ class TestsController < Simpler::Controller
 
   def index
     @time = Time.now
+    render plain: 'Plain text here'
   end
 
   def create
-
+    render plain: 'Status 201 here'
+    status 201 
   end
 
 end
